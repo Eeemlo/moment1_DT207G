880ee5e7653c743d4ab284635d1171e51195e2b7
Eeemlo <emma.lorensson@outlook.com>
2024-03-27 15:23:23 +0100

    Kopplat upp till databas

--- install.js
+++ install.js
@@ -1,4 +1,40 @@
+const { Client } = require("pg");
+require("dotenv").config();
 
+//Anslut till databas
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect((err) => {
+    if(err) {
+        console.log("fel vid anslutning" + err)
+    } else {
+        console.log("ansluten till databasen...")
+    }
+});
+
+//Skapa tabell
+client.query(`
+    CREATE TABLE courses(
+        id SERIAL PRIMARY KEY,
+        coursecode VARCHAR(5) NOT NULL,
+        coursename VARCHAR(100) NOT NULL,
+        syllabus VARCHAR(255) NOT NULL,
+        progresson CHAR(1) NOT NULL,
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
+
+/*
 //MYSQL
 const mysql = require("mysql");
 
@@ -44,4 +80,4 @@ connection.query(`INSERT INTO courses
 VALUES(?, ?, ?, ?);`, [], (err, results) => {
 if (err) throw err;
 console.table(results);
-});
+});*/
--- server.js
+++ server.js
@@ -1,24 +1,50 @@
+const { Client } = require("pg");
 const express = require('express');
-const bodyParser = require("body-parser"); //Möjlighet att läsa in formulärdata
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+
+/*const bodyParser = require("body-parser"); //Möjlighet att läsa in formulärdata*/ //Behövs inte nu?
+const port = process.env.PORT;
 let courseList = [];
 
 //Set view engine
 app.set('view engine', 'ejs');
 app.use(express.static("public"));  //Statiska filer i "public"
-app.use(bodyParser.urlencoded({extended: true})); //Ta emot formulärdata
+app.use(express.urlencoded({extended: true})); //Ta emot formulärdata
+
+
+//Anslut till databas
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect((err) => {
+    if(err) {
+        console.log("fel vid anslutning" + err)
+    } else {
+        console.log("ansluten till databasen...")
+    }
+});
+
 
 //Routing
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
     res.render("index");
 });
 
-app.get("/form", (req, res) => {
+app.get("/form", async(req, res) => {
     res.render("form");
 });
 
-app.post("/form", (req, res) => {
+app.post("/form", async(req, res) => {
         //Läs in formulärdata
         let newCourseName = req.body.coursename;
         let newCourseCode = req.body.coursecode;
@@ -35,7 +61,7 @@ app.post("/form", (req, res) => {
     res.render("form")
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", async(req, res) => {
     res.render("about");
 });
 
@@ -45,29 +71,3 @@ app.listen(port, () => {
 });
 
 
-/*
-//MYSQL
-const mysql = require("mysql");
-
-//Anslutningsinställningar
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "course-project",
-    password: "password",
-    database: "course-project"
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.log("connection failed" + err)
-        return;
-    }
-    console.log("connected to mysql")
-});
-
-connection.query(`INSERT INTO courses
-(coursecode, coursename, syllabus, progression)
-VALUES(?, ?, ?, ?);`, [], (err, results) => {
-if (err) throw err;
-console.table(results);
-});*/
