b95159489755fe56667adcb20bda961573606654
Eeemlo <emma.lorensson@outlook.com>
2024-03-28 11:26:13 +0100

    Lagt till funktionalitet för att radera + felmeddelande

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
 
 const app = express();
@@ -8,10 +8,9 @@ const app = express();
 const port = process.env.PORT;
 
 //Set view engine
-app.set('view engine', 'ejs');
-app.use(express.static("public"));  //Statiska filer i "public"
-app.use(express.urlencoded({extended: true})); //Ta emot formulärdata
-
+app.set("view engine", "ejs");
+app.use(express.static("public")); //Statiska filer i "public"
+app.use(express.urlencoded({ extended: true })); //Ta emot formulärdata
 
 //Anslut till databas
 const client = new Client({
@@ -26,55 +25,84 @@ const client = new Client({
 });
 
 client.connect((err) => {
-    if(err) {
-        console.log("fel vid anslutning" + err)
+    if (err) {
+        console.log("fel vid anslutning" + err);
     } else {
-        console.log("ansluten till databasen...")
+        console.log("ansluten till databasen...");
     }
 });
-
 
 //Routing
 app.get("/", async (req, res) => {
     // Läs ut från databasen
-    client.query("SELECT * FROM courses ORDER BY created DESC", (err, result) => {
-        if (err) {
-            console.log("fel vid db-fråga");
-        } else {
-            res.render("index", {
-                courses: result.rows
-            });
+    client.query(
+        "SELECT * FROM courses ORDER BY created DESC",
+        (err, result) => {
+            if (err) {
+                console.log("fel vid db-fråga");
+            } else {
+                res.render("index", {
+                    courses: result.rows,
+                });
+            }
         }
+    );
+});
+
+app.get("/form", async (req, res) => {
+    res.render("form", {
+        error: ""
     });
 });
 
-app.get("/form", async(req, res) => {
-    res.render("form");
-});
+app.post("/form", async (req, res) => {
+    //Läs in formulärdata
+    let newCourseCode = req.body.coursecode;
+    let newCourseName = req.body.coursename;
+    let newSyllabus = req.body.syllabus;
+    let newProgression = req.body.progression;
+    let error = "";
 
-app.post("/form", async(req, res) => {
-
-        //Läs in formulärdata
-        let newCourseCode = req.body.coursecode;
-        let newCourseName = req.body.coursename;
-        let newSyllabus = req.body.syllabus;
-        let newProgression = req.body.progression;
-
-        //SQL-frågor
-        const result = await client.query("INSERT INTO courses(coursecode, coursename, syllabus, progression) VALUES($1,$2,$3,$4)",
-        [newCourseCode, newCourseName, newSyllabus, newProgression]
+    if (
+        newCourseCode != "" &&
+        newCourseName != "" &&
+        newSyllabus != "" &&
+        newProgression != ""
+    ) {
+        //Lagra i databas
+        const result = await client.query(
+            "INSERT INTO courses(coursecode, coursename, syllabus, progression) VALUES($1,$2,$3,$4)",
+            [newCourseCode, newCourseName, newSyllabus, newProgression]
         );
 
         res.redirect("/");
+    } else {
+        error = "Samtliga fält måste vara ifyllda";
+
+        res.render("form", {
+            error: error
+        });
+    };
+
 });
 
-app.get("/about", async(req, res) => {
+app.get("/about", async (req, res) => {
     res.render("about");
+});
+
+//Radera inlägg
+app.get("/delete/:id", (req, res) => {
+    let id = req.params.id;
+
+    client.query("DELETE FROM courses WHERE id=$1;", [id], (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+        res.redirect("/");
+    });
 });
 
 //Starta
 app.listen(port, () => {
     console.log("Server started on port:" + port);
 });
-
-
